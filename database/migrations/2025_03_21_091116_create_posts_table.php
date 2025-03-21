57effaab9a3c4c9a94a4306610fc53ee05666c8f
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-21 18:27:46 +0500

    added four table name,email,password and image

--- database/migrations/2025_03_21_091116_create_posts_table.php
+++ database/migrations/2025_03_21_091116_create_posts_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique(); 
+            $table->string('password');
+            $table->string('image')->nullable()->change();
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('users');
     }
 };
