f35f90294f9783d22e3f66a75ede422751ed0026
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-21 18:38:43 +0500

    added five more routes after first route and  each one for one methods inside of the UserController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('register');
 });
 
 
-Route::resource('posts',PostController::class)->names('posts');
+Route::resource('users',UserController::class)->names('users');
+Route::get('/logout',[UserController::class,'logout'])->name('logout');
+Route::post('/login',[UserController::class,'login'])->name('login');
+Route::get('/handlelogin',[UserController::class,'handleLogin'])->name('handleLogin');
+Route::post('/register',[UserController::class,'register'])->name('register');
+Route::put('/users/{post}', [UserController::class, 'update'])->name('users.update');
