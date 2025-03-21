cc2c23f4fc0208a194271b24737212936555047e
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-21 18:35:35 +0500

    changed linkis, added alert-danger that show erors and added title section that change title when this page opened

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,41 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <title>Register</title>
-</head>
-
-<body>
+@extends('layouts.app')
+@section('title','Register')
+@section('content')
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <h2 class="text-center">Register</h2>
-                <form>
+                <form action="{{route('register')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
                         <label for="registerName">Name</label>
-                        <input type="text" class="form-control" id="registerName" required>
+                        <input type="text" class="form-control" id="registerName" name="name">
                     </div>
                     <div class="form-group">
                         <label for="registerEmail">Email address</label>
-                        <input type="email" class="form-control" id="registerEmail" required>
+                        <input type="email" class="form-control" id="registerEmail" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="Password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
                     </div>
                     <div class="form-group">
                         <label for="registerAvatar">Upload Avatar</label>
-                        <input type="file" class="form-control-file" id="registerAvatar">
+                        <input type="file" class="form-control-file" id="registerAvatar" name="image">
                     </div>
                     <button type="submit" class="btn btn-success btn-block">Register</button>
                 </form>
-                <p class="text-center mt-2">Already have an account? <a href="login.html">Login</a></p>
+                <p class="text-center mt-2">Already have an account? <a href="{{route('handleLogin')}}">Login</a></p>
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+@endif
+@endsection
