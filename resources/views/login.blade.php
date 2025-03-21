c4a31f7bffd126f0bcae640357a8a07cbf9f134f
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-21 18:34:13 +0500

    changed linkis, layouted navbar, added alert-danger that show erors

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,37 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <title>Login</title>
-</head>
-
-<body>
+@extends('layouts.app')
+@section('title','Login')
+@section('content')
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-6 offset-md-3">
                 <h2 class="text-center">Login</h2>
-                <form>
+                <form action="{{route('login')}}" method="POST" enctype="multipart/form-data">
+                @csrf
                     <div class="form-group">
                         <label for="loginEmail">Email address</label>
-                        <input type="email" class="form-control" id="loginEmail" required>
+                        <input type="email" class="form-control" id="loginEmail" name="email">
                     </div>
                     <div class="form-group">
                         <label for="loginPassword">Password</label>
-                        <input type="password" class="form-control" id="loginPassword" required>
+                        <input type="password" class="form-control" id="loginPassword" name="password">
                     </div>
                     <button type="submit" class="btn btn-success btn-block">Login</button>
                 </form>
-                <p class="text-center mt-2">Don't have an account? <a href="register.html">Register</a></p>
+                <p class="text-center mt-2">Don't have an account? <a href="{{route('users.create')}}">Register</a></p>
             </div>
         </div>
     </div>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+@endif
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+@endsection
